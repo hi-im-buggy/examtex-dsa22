7a9cb7cb791b910d09dbb697e3a72d64bd1d33b6
Pratyaksh Gautam <pratyakshgautam11@gmail.com>
2021-05-09 16:52:36 +0530

    Add parser for user parameters

--- src/listwrapper.h
+++ src/listwrapper.h
@@ -3,6 +3,7 @@
 // Will generate a set of wrapper functions given the data type
 
 #include "list.h"
+#include "interpreter.h"
 
 #define CAT_EXPAND(a, b) a##b
 #define CAT(a, b)                                                              \
@@ -34,12 +35,6 @@
 // declaration of functions. Make sure that the specidied data type is
 // accessible from this file (include if necessary)
 GENERATE_LIST_WRAPPER(int);
-
-typedef struct __test_struct {
-	int a;
-	char c;
-} test_struct;
-
-GENERATE_LIST_WRAPPER(test_struct);
+GENERATE_LIST_WRAPPER(User_Parameters);
 
 #endif // __INCLUDE_LIST_WRAPPER_H
